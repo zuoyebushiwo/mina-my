7944b021423212399df3512527b72c8b3312a9f5
zhangxuej <957168943@qq.com>
2015-05-11 21:44:37 +0800

    Change-Id: Ibdb120ace5961c1f18e54cb6b347d9c3c8286878

---
+++ src/main/java/org/apache/mina/transport/socket/AbstractDatagramSessionConfig.java
@@ -0,0 +1,10 @@
+package org.apache.mina.transport.socket;
+
+public class AbstractDatagramSessionConfig {
+
+	public boolean isCloseOnPortUnreachable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/org/apache/mina/transport/socket/nio/NioSocketConnector.java
@@ -0,0 +1,5 @@
+package org.apache.mina.transport.socket.nio;
+
+public class NioSocketConnector {
+
+}
